4644dfb8581eefb30f641c5834100b7a47e88a21
Pedro Azeredo <pedro.azeredo93@gmail.com>
2021-01-22 01:43:23 -0300

    Criado cadastro de usuarios com RestOpartor

--- restOperator.js
+++ restOperator.js
@@ -3,3 +3,17 @@ function minhaLista(...nomes){
 }
 
 minhaLista("Pedro", "Francesca", "Melissa");
+
+//Criando cadastro de usuarios
+function cadastrar(usuarios, ...novosUsuarios){
+    let totalUsuarios = [
+        ...usuarios,
+        ...novosUsuarios
+    ];
+
+    return console.log(totalUsuarios);
+}
+
+let usuarios = ["Pedro", "Ander"];
+
+let novosUsuarios = cadastrar(usuarios, "Pablo", "Yuri");
