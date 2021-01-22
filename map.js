6628fc7f3505b21a200d68840f151079c934c7cd
Pedro Azeredo <pedro.azeredo93@gmail.com>
2021-01-22 01:55:35 -0300

    Trabalhando com funçoes map, reduce e find

---
+++ map.js
@@ -0,0 +1,20 @@
+const lista = [1,2,3,4,5,6];
+
+//map
+const novaLista = lista.map(function(item){
+    return item * 5;
+});
+
+console.log(novaLista);
+
+//reduce
+const soma = lista.reduce(function(total, proximo){
+    return total + proximo;
+});
+
+//find
+const busca = lista.find(function(item){
+    return item === 6;
+});
+
+console.log(busca);
