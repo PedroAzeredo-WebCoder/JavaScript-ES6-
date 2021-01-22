42e6b7bdd713c478de2c4cb8db54daaffd00f780
Pedro Azeredo <pedro.azeredo93@gmail.com>
2021-01-22 02:09:35 -0300

    Utilizando includes em arrays

---
+++ includes.js
@@ -0,0 +1,9 @@
+let nomes = ['Pedro','Fábio','Vítor'];
+
+console.log(nomes.includes('Pedro'));
+
+if(nomes.includes('Fábio')){
+    console.log('Fábio está na lista');
+}else{
+    console.log('Fábio não está na lista');
+}
